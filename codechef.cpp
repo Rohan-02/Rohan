f71b4f34e2777510bf6f0ad0bab276494181c076
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2020-08-23 23:26:11 +0530

    Update codechef.cpp
    check it once

--- codechef.cpp
+++ codechef.cpp
@@ -1,33 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
+int main(){
+	int t;  cin>>t;
+	while(t--){
 	    int n,k,ans=0;
 	    cin>>n>>k;
-	    int a;
-	    for(int i=0;i<n;i++)
-	    {
-	        cin>>a;
-	    }
-	    for(int i=0;i<n;i++)
-	    {
-	        if(a>k)
-	        {
+	    int a[n];
+	    for(int i=0;i<n;cin>>a[i++])
+	    for(int i=0;i<n;i++){
+	        if(a[i]>k)
 	            ans--;
-	        }
 	        else
-	        {
 	            ans=ans+a;
-	        }
 	    }
 	    if(ans<0)
-	    cout<<"-1"<<endl;
+		    cout<<"-1"<<endl;
 	    else
-	    cout<<abs(ans-k-1)<<endl;
+		    cout<<abs(ans-k-1)<<endl;
 	}
 	return 0;
 }
