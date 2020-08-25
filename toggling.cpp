01a0bf91ddd164882df5eb83bb5d74aea5da9452
Avinash Sharma <58262578+avi9360@users.noreply.github.com>
2020-08-25 13:38:31 +0530

    Update toggling.cpp
    1.0

--- toggling.cpp
+++ toggling.cpp
@@ -1,26 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 	string s;
-	while(cin>>s)
-	{
-		for(int i=0;i<s.size();i++)
-		{
+	while(cin>>s){
+		for(int i=0;i<s.size();i++){
 			if(isupper(s[i]))
-			{
 				s[i]=toupper(s[i]);
-			}
 			else if(islower(s[i]))
-			{
 				s[i]=tolower(s[i]);
-			}
 			else
-			{
 				s[i]=s[i];
-			}
 		}
 		cout<<s;
 	}
-	return 0;
+return 0;
 }
